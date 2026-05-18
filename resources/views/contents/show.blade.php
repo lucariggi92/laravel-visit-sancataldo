774eb6099017d3bdb1c85a25db1696176723536e
lucariggi92 <riggiluca92@gmail.com>
2026-05-18 21:19:58 +0200

    feat: modal detroy

--- resources/views/contents/show.blade.php
+++ resources/views/contents/show.blade.php
@@ -14,19 +14,19 @@
 
             <div class="d-flex gap-2">
                 <a href="{{route("admin.contents.edit", $content)}}" class="btn btn-sm btn-warning text-white px-3 py-2 d-flex align-items-center shadow-sm" 
-                  font-size: 0.85rem; border-radius: 6px; font-weight: 500;">
+                  style="font-size: 0.85rem; border-radius: 6px; font-weight: 500;">
                    Modifica Contenuto
                 </a>
-                
-                <form action="{{route("admin.contents.destroy", $content)}}" method="POST">
-                    @csrf
-                    @method("DELETE")
-                    <input type="submit" class="btn btn-sm btn-danger px-3 py-2 d-flex align-items-center shadow-sm" 
-                        style="font-size: 0.85rem; border-radius: 6px; font-weight: 500;" value="Elimina">
-           
 
-                </form>
-               
+
+                <button type="button" class="btn btn-sm btn-danger px-3 py-2 d-flex align-items-center shadow-sm" 
+                style="font-size: 0.85rem; border-radius: 6px; font-weight: 500;"
+                 data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Elimina
+                </button>
+
+
+                
             </div>
         </div>
 
@@ -75,6 +75,35 @@
         </div>
 
     </div>
+</div>
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina il contenuto</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      Sei sicuro di voler eliminare <strong>{{ $content->title }}</strong>? L'operazione non è reversibile.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+        <form action="{{route("admin.contents.destroy", $content)}}" method="POST">
+                    @csrf
+                    @method("DELETE")
+                    <input type="submit" class="btn btn-sm btn-danger px-3 py-2 d-flex align-items-center shadow-sm" 
+                        style="font-size: 0.85rem; border-radius: 6px; font-weight: 500;" value="Elimina">
+           
+
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
 
 @endsection
